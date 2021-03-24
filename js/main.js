15e5c98d2c913d0714ea393d1e766133ab1225de
Dil Somb <dils@Dils-MacBook-Pro.local>
2021-03-23 17:03:04 -0700

    format

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
 function init() {
-    // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
-    let btnAddEmployee = document.getElementById('btnAddEmployee');
-    // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
-    btnAddEmployee.addEventListener('click', () => {
-        window.open('./add-employee.html', "addPopup", "width=400, height=600");
-    })
+  // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
+  let btnAddEmployee = document.getElementById("btnAddEmployee");
+  // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
+  btnAddEmployee.addEventListener("click", () => {
+    window.open("./add-employee.html", "addPopup", "width=400, height=600");
+  });
 }
 
-window.addEventListener('load', init)
+window.addEventListener("load", init);
